43b2d9d64795d4bfdca6ade3eec79998e8380b54
elektrick66 <louaymoussa2001@gmail.com>
2023-05-31 01:05:53 +0100

    last changes with swagger api

--- server.js
+++ server.js
@@ -7,6 +7,31 @@ const transactions_routes = require("./routes/transactions")
 
 require('dotenv').config()
 
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'Express API for retrieving data from mongodb',
+    version: '1.0.0',
+  },
+  servers: [
+    {
+      url: 'http://localhost:3000',
+      description: 'Development server',
+    },
+  ],
+};
+
+const options = {
+  swaggerDefinition,
+  // Paths to files containing OpenAPI definitions
+  apis: ['./routes/*.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
 app.listen(process.env.PORT || 3000,()=>{
     console.log(`Server running on port ${process.env.PORT}`)
 })
@@ -18,3 +43,4 @@ app.use(express.json())
 
 app.use('/api/orders', orders_routes)
 app.use('/api/transactions', transactions_routes)
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
